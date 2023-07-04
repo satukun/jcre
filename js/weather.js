4daa1c82b72af795536912cebc2b85175f2ad773
isomura <0594isomura@gmail.com>
2023-07-04 15:31:33 +0900

    変更

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,29 @@
-$(function () {});
+$(function () {
+
+  function checkBreakPoint() {
+    w = $(window).width();
+    if (w <= 1280) {
+      // 1280px以下のとき
+      $('#slider-weatherForecastData').not('.slick-initialized').slick({
+        //スライドさせる
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      });
+    } else {
+      // 1281以上のとき
+      //スライドさせない
+      $('#slider-weatherForecastData').slick('unslick');
+    }
+    if (w <= 768) {
+      // 768px以下のとき
+      //スライドさせない
+      $('#slider-weatherForecastData').slick('unslick');
+    } 
+  }
+  // ウインドウがリサイズする度にチェック
+  $(window).resize(function () {
+    checkBreakPoint();
+  });
+  // 初回チェック
+  checkBreakPoint();
+});
