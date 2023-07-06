280f3c488d5f7cdb2b6d88f272501460e43e0a84
isomura <0594isomura@gmail.com>
2023-07-06 12:33:02 +0900

    %→px スライダー枚数調整

--- js/weather.js
+++ js/weather.js
@@ -6,13 +6,13 @@ $(function () {
       // 1280px以下のとき
       $('#slider-weatherForecastData').not('.slick-initialized').slick({
         //スライドさせる
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
           {
-            breakpoint: 1024, // 399px以下のサイズに適用
+            breakpoint: 1024,
             settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             },
           },
         ],
